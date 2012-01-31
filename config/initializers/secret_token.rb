a82b44a4cb4f479d60287e1d8fa1bbe2c9eb966b
unknown <CiZ@.(none)>
2012-01-31 23:11:24 +0700

    change root to secondproj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secondproj::Application.config.secret_token = '2fe1ec1fd3c62233765f5243aff216bf40743d6455f1e08e805acf85c64e5b8abadfa1a3f865b57e836e745fbff381f0e652b63353a120286187e7433ca563de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Secondproj::Application.config.session_store :cookie_store, key: '_secondproj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Secondproj::Application.config.session_store :active_record_store
